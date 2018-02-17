633bd3c304a8e9530194e0f78d09dd9a74b06dab
LEGAGNEUR Matthieu <legagneur.matthieu@gmail.com>
2018-02-17 12:14:03 +0100

    MySH : init

---
+++ MySh.sh
@@ -0,0 +1,132 @@
+#! /bin/sh
+
+# Copyright 2018 LEGAGNEUR Matthieu
+# GNU GPL V3 https://www.gnu.org/licenses/gpl.txt
+
+mysh_read=0
+mysh_var_exit=0
+mysh_var_uptodate=0
+mysh_var_env_base=0
+
+mysh_main(){
+	clear
+	if test "$USER" != "root"
+	then
+		echo "MySH require sudo / root !"
+		mysh_var_exit=1
+	fi
+	while test $mysh_var_exit -eq 0
+	do 
+		clear
+		mysh_menu 
+	done
+}
+
+mysh_menu(){
+	echo "Welcome in MySH by LEGAGNEUR Matthieu\n"
+	echo "1) Update Ubuntu"
+	echo "2) Install env base"
+	echo "3) Install env gamer"
+	echo "4) Install env phpdev"
+	echo "5) Fix audio"
+	echo "6) Install pkg OpenOffice"
+	echo "7) Install pkg compiz"
+	echo "0) Exit"
+	echo "Enter a number :"
+	read mysh_read
+	case $mysh_read in
+		1) mysh_update;;
+		2) mysh_install_env_base;;
+		3) mysh_install_env_gamer;;
+		4) mysh_install_env_phpdev;;
+		5) mysh_fix_audio;;
+		6) mysh_pkg_OpenOffice;;
+		7) mysh_pkg_compiz;;
+		0) mysh_exit;;
+	esac
+}
+
+mysh_press_enter(){
+	if test $mysh_read -eq $1
+	then
+		echo "\nPress ENTER for back to menu"
+		read mysh_useless
+	fi
+}
+
+mysh_is_uptodate(){
+	if test $mysh_var_uptodate -eq 0
+	then
+		mysh_update
+	fi
+}
+
+mysh_base_instaled(){
+	if test $mysh_var_env_base -eq 0
+	then
+		mysh_install_env_base
+	fi
+}
+
+mysh_update(){
+	apt-get update
+	apt-get -y upgrade
+	apt-get -y dist-upgrade
+	apt-get -y autoremove
+	do-release-upgrade
+	mysh_var_uptodate=1
+	mysh_press_enter 1
+}
+
+mysh_install_env_base(){
+	mysh_is_uptodate
+	apt-get -y install clamav clamav-daemon default-jre default-jdk firefox-locale-fr thunderbird-locale-fr
+	mysh_var_env_base=1
+	mysh_press_enter 2
+}
+
+mysh_install_env_gamer(){
+	mysh_base_instaled
+	apt-get -y install playonlinux steam
+	mysh_press_enter 3
+}
+
+mysh_install_env_phpdev(){
+	mysh_base_instaled
+	apt-get -y install tasksel netbeans fail2ban git
+	tasksel
+	apt-get -y install phpmyadmin
+	mysh_press_enter 4
+}
+
+mysh_fix_audio(){
+	mysh_is_uptodate
+	apt-get -y install alsa alsa-* pulseaudio 
+	mysh_press_enter 5
+}
+
+mysh_pkg_OpenOffice(){
+	mkdir AOO/
+	cd AOO/
+	wget https://netix.dl.sourceforge.net/project/openofficeorg.mirror/4.1.5/binaries/fr/Apache_OpenOffice_4.1.5_Linux_x86-64_install-deb_fr.tar.gz -O AOO.tar.gz
+	tar xvf AOO.tar.gz
+	dpkg -i fr/DEBS/*.deb
+	dpkg -i fr/DEBS/desktop-integration/*.deb
+	cd ../
+	rm -fr AOO/
+	mysh_press_enter 6
+}
+
+mysh_pkg_compiz(){
+	mysh_base_instaled
+	apt-get -y install compiz compizconfig-settings-manager compiz-plugins compiz-plugins-extra compiz-plugins-main
+	mysh_press_enter 7
+}
+
+mysh_exit(){
+	clear
+	echo "MySH : Good Bye !"
+	mysh_var_exit=1
+}
+
+mysh_main
---
+++ README.md
@@ -0,0 +1,45 @@
+﻿# MySH
+
+MySH est un script permettant à la suite d'une installation d'Ubuntu (ou variantes) d'installer facilement et rapidement :
+- un environnement de base 
+- un environnement joueur
+- un environnement développeur PHP
+- Résoudre les problèmes audio (courant sur Lubuntu)
+- La suite burautique OpenOffice (si LibreOffice ne vous convient pas)
+
+MySH permet également de lancer une mise à jour complète du système
+
+## Lancer MySH
+
+Dans votre console tapez :
+
+<code>sudo sh [chemain]/MySH.sh</code>
+
+et faites votre choix.
+
+## Installation environnement de base
+
+Lance les mise a jour si vous ne l'avez pas fait au préalable et installe les paquets suivants :
+
+- clamav clamav-daemon : Antivirus Libre
+- default-jre default-jdk : Java
+- firefox-locale-fr thunderbird-locale-fr : Navigateur et client de messagerie en français
+
+## Installation environnement de joueur
+
+Lance l'installation de l'environement de base si vous ne l'avez pas fait au préalable et installe les paquets suivants :
+
+- playonlinux et steam : Bibliothèque de jeux
+
+## Installation environnement développeur PHP
+
+Lance l'installation de l'environnement de base si vous ne l'avez pas fait au préalable et installe les paquets suivants :
+
+- tasksel : Bibliothèque de serveurs (pour installer LAMP)
+- netbeans : IDE
+- git : Verssioning
+- fail2ban : sécurité
+- phpmyadmin : Administration de MySQL
+
+Lors de l'installation tasksel et phpmyadmin vous demanderont des détails ds configuration,
+sélectionnez "LAMP Setver", "Apache" et saisissez les mots de passe qui sécuriseront votre base MySQL
